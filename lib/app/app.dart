d40eb340dadfe12050245554dd7497986a556ffc
HarshaL181818 <143408321+HarshaL181818@users.noreply.github.com>
2024-07-21 15:30:47 +0530

    Initial Commit

---
+++ lib/app/app.dart
@@ -0,0 +1,15 @@
+
+import 'package:first_flutter/views/home_view.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget{
+  const MyApp({
+    super.key,
+  });
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: "Flutter",
+      home: HomeView(),
+    );
+  }
+}
